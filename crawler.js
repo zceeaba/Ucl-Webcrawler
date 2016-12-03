7d2da307c5cf4781c2c3ad84c5db3a2e9912398b
zcabzjs <zijian12345@hotmail.com>
2016-12-03 15:40:40 +0000

    Add files via upload

--- crawler.js
+++ crawler.js
@@ -1,18 +1,25 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
-var prompt = require('prompt');
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
 
-var START_URL = prompt('What are you looking for?');
-var SEARCH_WORD = "stemming";
 var MAX_PAGES_TO_VISIT = 10;
 
 var pagesVisited = {};
 var numPagesVisited = 0;
 var pagesToVisit = [];
+app.use(bodyParser.urlencoded({extended:true}));
+app.get('/', function(req,res){
+  res.sendFile(__dirname + '/view.html' );
+});
+
+app.post('/myaction', function(req,res){
+var START_URL = req.body.url;
+var SEARCH_WORD = req.body.keyword;
 var url = new URL(START_URL);
 var baseUrl = url.protocol + "//" + url.hostname;
-
 pagesToVisit.push(START_URL);
 crawl();
 
@@ -70,3 +77,11 @@ function collectInternalLinks($) {
         pagesToVisit.push(baseUrl + $(this).attr('href'));
     });
 }
+});
+
+app.listen(8080, function(){
+  console.log('Server running at http://127.0.0.1:8080/');
+
+});
+
+
